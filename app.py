a60166ac23ac8dacec73872c620eb91fe8857dc6
Om Dusane <omdusane8@gmail.com>
2023-12-26 16:02:08 +0530

    Added html files and routes

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return "Home page"
+    return render_template("home.html")
+
+@app.route("/about-us", methods=['GET'])
+def about_us():
+    return render_template("about-us.html")
+
+@app.route("/contact-us", methods=['GET', 'POST'])
+def contact_us():
+
+    if request.method == "POST":
+        name = request.form["name"]
+
+        flash("Form Submitted Successfully")
+        return redirect(url_for(contact_us))
+
+    return render_template("contact-us.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
